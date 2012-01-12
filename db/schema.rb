07de96cff599707798a63c2732cfe36090b863e8
snandipa <mnandipa@gmail.com>
2012-01-12 14:12:18 -0500

    Created users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120112190648) do
+ActiveRecord::Schema.define(:version => 20120112190902) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -22,5 +22,7 @@ ActiveRecord::Schema.define(:version => 20120112190648) do
     t.boolean  "admin",              :default => false
     t.string   "salt"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
