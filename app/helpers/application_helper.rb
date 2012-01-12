42df28c53424bd62cc629626f4b2ae60956ce0e1
snandipa <mnandipa@gmail.com>
2012-01-12 14:11:28 -0500

    Created users model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def title
+    base_title = "Penn Schedule Assembler"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+class User < ActiveRecord::Base
+  attr_accessor   :password
+  attr_accessible :name, :email, :password, :password_confirmation
+  
+  email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  
+  validates_presence_of :email, :name, :password, :password_confirmation
+  validates_length_of :name, {:maximum => 50}
+  validates_length_of :password, {:within => 6..40}
+  validates_format_of :email, {:with => email_regex}
+  validates_uniqueness_of :email, {:case_sensitive => false}
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  class << self
+    def authenticate(email, submitted_password)
+      user = find_by_email(email)
+      (user && user.has_password?(submitted_password)) ? user : nil
+    end
+    
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+    end
+  end
+  
+  private
+  
+    def encrypt_password
+      self.salt = make_salt unless has_password?(password)
+      self.encrypted_password = encrypt(password)
+    end
+  
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+end
