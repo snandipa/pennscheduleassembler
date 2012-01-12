0ce867a45b0d341fc1406e2a0256307316abba55
snandipa <mnandipa@gmail.com>
2012-01-11 21:48:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pennscheduleassembler::Application.config.secret_token = 'f16a8e2e49c69136d51d325c21c654cb60dc70e981dc26386978406ffb1ef894d37626a4580ebd620a2edaeb51111fa0251df0a1e364e89f7f7cffe6331927d1'
