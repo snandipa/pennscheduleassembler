df6759c7c12f04d154934ee5d9fed281a5adb8f7
snandipa <mnandipa@gmail.com>
2012-02-14 10:17:23 -0500

    Completed Timings, Recitations, and Courses Scheduling

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -11,8 +11,6 @@ class SchedulesController < ApplicationController
     end
 
     combination_array = create_combinations_array
-    puts "%^%^%^%^%^%^^% combination array: #{combination_array}"
-
     any_created = false
     for i in (0..combination_array.length-1) #iterates through all Combination of Sections
       if combination_array[i].overlaps_with_itself? == false ##then you can create a schedule from this
@@ -38,6 +36,7 @@ class SchedulesController < ApplicationController
     num_sections = Array.new
     section_type = Array.new
     constraints = current_user.constraints #this is an Array of Constraints
+    timings = current_user.timings if current_user.timings.count > 0
     constraints.each do |constraint|
       num_sections << constraint.course.sections.count
       section_type << "Course"
@@ -53,7 +52,6 @@ class SchedulesController < ApplicationController
         section_type << "Timing"
       end
     end
-    puts "%^%^%^%^^%%^ section type array: #{section_type}"
     num_constraints = num_sections.length
     num_combinations = num_sections.inject(1) { |prod, element| prod * element }
     #period = num_combinations
@@ -61,35 +59,34 @@ class SchedulesController < ApplicationController
     #first arg is rows, second is cols
     combination_array = Array.new(num_combinations).map!{ Array.new(num_constraints) }
     
-    timing_counter=0
+    timing_counter=-1
     constraints_counter = 0
-    
+    total_periods = num_combinations
+    total_repeats = num_combinations
     for col in (0..num_constraints-1)
-      period = num_combinations/num_sections[col] - 1
+      total_repeats = total_repeats / num_sections[col]
+      total_periods = num_combinations/total_repeats
       combination_counter = 0
-      for next_unit in (0..num_sections[col]-1)
-        for repeat in (0..period)
-          puts "%^%^%^%^^%%^ #{section_type[col]} | col: #{col}"
+      for num_period in (0..total_periods-1)
+        for repeat in (0..total_repeats-1)
           case
             when section_type[col] == "Course"
-              combination_array[combination_counter][col]=constraints[constraints_counter].course.sections[next_unit]
+              combination_array[combination_counter][col]=constraints[constraints_counter].course.sections[num_period % num_sections[col]]
               combination_counter = combination_counter + 1
               
             when section_type[col] == "Recitation"
               if constraints[constraints_counter].course.recitations.count > 0
-                combination_array[combination_counter][col]=constraints[constraints_counter].course.recitations[next_unit]
+                combination_array[combination_counter][col]=constraints[constraints_counter].course.recitations[num_period % num_sections[col]]
                 combination_counter = combination_counter + 1
               end
             when section_type[col] == "Timing"
-                combination_array[combination_counter][col]=current_user.timings[timing_counter]
-                puts "%^%^%^%^^%%^ pudda in"
+                combination_array[combination_counter][col]=timings[timing_counter]
                 combination_counter = combination_counter + 1 
           end
         end  
       end
       
       constraints_counter = constraints_counter + 1 if (col+1 < num_constraints) && (section_type[col+1] == "Course")
-      puts "%^%^%^%^^%%^ after potential increment constraintscounter: #{constraints_counter}"
       timing_counter = timing_counter + 1 if (col+1 < num_constraints) && (section_type[col+1] == "Timing")
     end
     
